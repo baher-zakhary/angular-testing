9d693d07ef6975ee392a359a4f75e19e2d0af3ea
Baher Zakhary <baher.zakharyy@gmail.com>
2022-01-06 13:32:00 +0200

    testing components, set up and tear down

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,61 @@
 import { VoteComponent } from './vote.component'; 
 
+// describe('VoteComponent', () => {
+//   it('it should increment totalVotes when upvoted', () => {
+//     // Triple AAA structure (Arrange, Act, Assert)
+//     // Arrange
+//     const component = new VoteComponent();
+//     // Act
+//     component.upVote();
+//     // Assert
+//     expect(component.totalVotes).toBe(1);
+//   });
+
+//   it('it should decrement totalVotes when downvoted', () => {
+//     // ALWAYS START WITH A CLEAN STATE (EACH TEST SHOULD RUN IN ISOLATION WITH AFFECTING OR BEING AFFECTED BY OTHER TESTS)
+//     const component = new VoteComponent();
+    
+//     component.downVote();
+    
+//     expect(component.totalVotes).toBe(-1);
+//   });
+// });
+
+
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+
+  // ALWAYS START WITH A CLEAN STATE (EACH TEST SHOULD RUN IN ISOLATION WITH AFFECTING OR BEING AFFECTED BY OTHER TESTS)
+  beforeEach(() => {
+    // Arrange
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  afterEach(() => {
+    // any clean up code after each test
+    // (Set up code)
+  });
+
+  beforeAll(() => {
+    // executed once before all tests
+    // (Tear down code)
+  });
+
+  afterAll(() => {
+    // executed once after all tests
+  });
+
+  it('it should increment totalVotes when upvoted', () => {
+    // Act
+    component.upVote();
+    // Assert
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('it should decrement totalVotes when downvoted', () => {
+    // Act
+    component.downVote();
+    // Assert
+    expect(component.totalVotes).toBe(-1);
   });
 });
