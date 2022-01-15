8736b3e3633bcb60978165aaeb49aee093cd43e5
Baher Zakhary <baher.zakharyy@gmail.com>
2022-01-15 19:05:51 +0200

    Testing forms

--- src/app/04-forms/todo-form.component.spec.ts
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,23 @@
+import { FormBuilder } from '@angular/forms';
 import { TodoFormComponent } from './todo-form.component'; 
 
 describe('TodoFormComponent', () => {
-  var component: TodoFormComponent; 
+  let component: TodoFormComponent; 
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
+  it('should contain two controls', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
   });
 
-  it('', () => {
+  it('should make name control required', () => {
+    let control = component.form.get('name');
+    if (control) {
+      control.setValue('');
+      expect(control.valid).toBeFalsy();
+    }
   });
 });
