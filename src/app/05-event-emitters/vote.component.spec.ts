ab812e5a2fedf393cef60fee983ca763e5b1bb30
Baher Zakhary <baher.zakharyy@gmail.com>
2022-01-15 19:31:21 +0200

    testing event emitters

--- src/app/05-event-emitters/vote.component.spec.ts
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -7,6 +7,14 @@ describe('VoteComponent', () => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise voteChanged event when upvoted', () => {
+    let totalVotes = null;
+    // since event emitters are observables, we can subscribe to them
+    component.voteChanged.subscribe(newTotalVotes => totalVotes = newTotalVotes);
+    
+    component.upVote();
+    
+    // expect(totalVotes).not.toBeNull();
+    expect(totalVotes).toBe(1);
   });
 });
